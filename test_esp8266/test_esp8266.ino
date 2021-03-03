4de098953daa8a3d7286c60e552dee226c854898
Tuan Nguyen <Dungnhincainay@gmail.com>
2021-03-04 00:35:27 +0700

    BLYNK WRITE

--- test_esp8266/test_esp8266.ino
+++ test_esp8266/test_esp8266.ino
@@ -1,66 +1,94 @@
-
 #define BLYNK_PRINT Serial
-
-
 #include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+#include <BlynkSimpleEsp8266.h>    
 //#include <SimpleTimer.h>
-
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
+ 
+SimpleTimer timer;
+//char auth[] = "C39HJavn-IoVsrHbYA7ea3IqlKhGauFL";
 char auth[] = "8TytNxie25BVKYolzRm_yLjF1vAGbrCA";
-
 char ssid[] = "Wifi Cua Tuan";
 char pass[] = "";
 
 float t=0;
 float h=0;
+int flag=0;
 
-WidgetLED led1(V1);
-
-BlynkTimer timer;
-
-// V1 LED Widget is blinking
-void blinkLedWidget()
-{
-  if (led1.getValue()) {
-    led1.off();
-    Serial.println("LED on V1: off");
-  } else {
-    led1.on();
-    Serial.println("LED on V1: on");
-  }
-
-  h = h+0.845 ;
-  t = t+1.23 ;
-  if (h>=100){
-    h=0;
-  }
-  if(t>=100){
-    t=1;
-  }
-  Serial.println("Ket qua do duoc:\n");
-  Serial.print("Do Am = ");
-  Serial.print(h);
-  Serial.print("%  ");
-  Serial.print("Nhiet do = ");
-  Serial.print(t); 
-  Serial.print("°C \n");
-  Blynk.virtualWrite(V6, t);
-  Blynk.virtualWrite(V5, h);
-  
-}
+/*
+ * V1 V2 : Chọn chế độ
+ * v7 Kp
+ * V8 Ki
+ * V9 Kd
+ */
+int ledState = LOW;
+float value1;
+unsigned long Ago = 0;
+unsigned long Now = 0;
+const long Set_time = 300;
 
 void setup()
 {
+    Serial.begin(115200);
+    Blynk.begin(auth, ssid, pass,"iot.htpro.vn", 8080);
+    pinMode(LED_BUILTIN, OUTPUT);
+    timer.setInterval(500, sendUptime);
+}
+BLYNK_CONNECTED() {
+    Blynk.syncAll();
+}
+BLYNK_WRITE(V1)
+{   
+  value1 = param.asFloat(); // Get value as integer
+  // The param can contain multiple values, in such case:
+  float x = param[0].asInt();
+  float y = param[1].asInt();
+//  Serial.print("\nGia tri V1: ");                  // Hiển thị trên monitor
+//  Serial.print(value1);
+//  Serial.print(" ");                  // Hiển thị trên monitor
+//  Serial.print(x);
+//  Serial.print(" ");                  // Hiển thị trên monitor
+//  Serial.print(y);
+//  delay(1500);
+}
+void sendUptime()
+{
+  h = h+0.845 ;             // Đọc giá tri nhiệt độ, độ ẩm không khí
+  t = t+1.23 ;              // Đọc giá tri nhiệt độ, độ ẩm không khí
+  if (h>=100){              // Đọc giá tri nhiệt độ, độ ẩm không khí
+    h=0;                    // Đọc giá tri nhiệt độ, độ ẩm không khí
+  }                         // Đọc giá tri nhiệt độ, độ ẩm không khí
+  if(t>=100){               // Đọc giá tri nhiệt độ, độ ẩm không khí
+    t=1;                    // Đọc giá tri nhiệt độ, độ ẩm không khí
+  }
+  Now = millis();                 // Nhấp nháy led
+  if (Now - Ago >= Set_time) {    // Nhấp nháy led
+    Ago = Now;                    // Nhấp nháy led
+    if (ledState == LOW) {        // Nhấp nháy led
+      ledState = HIGH;            // Nhấp nháy led
+    } else {                      // Nhấp nháy led
+      ledState = LOW;             // Nhấp nháy led
+    }                             // Nhấp nháy led
+    digitalWrite(LED_BUILTIN, ledState);       // Đảo trạng thái led
+  }
+  Serial.println("\nKet qua do duoc:");       // Hiển thị trên monitor
+  Serial.print("Do Am = ");                     // Hiển thị trên monitor
+  Serial.print(h);                              // Hiển thị trên monitor
+  Serial.print("%  ");                          // Hiển thị trên monitor
+  Serial.print("Nhiet do = ");                  // Hiển thị trên monitor
+  Serial.print(t);                              // Hiển thị trên monitor
+  Serial.print("°C \n");
+  Serial.print("\nGia tri V1: ");                  // Hiển thị trên monitor
+  Serial.print(value1);                           // Hiển thị trên monitor
+  Serial.print(" ");                  // Hiển thị trên monitor
 
-  Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass,"iot.htpro.vn", 8080);
-  //Blynk.begin(auth, ssid, pass);
-  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
-  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
-
-  timer.setInterval(1000L, blinkLedWidget);
+  Blynk.virtualWrite(V6, t);      // Gửi dữ liệu lên sever - lên Điện thoại.
+  Blynk.virtualWrite(V5, h);      // Gửi dữ liệu lên sever - lên Điện thoại.
+  
+  if(t>=70 && flag==0){
+    Blynk.notify("Nhiệt độ cao");
+    flag=1;
+  }else if(t<70){
+      flag=0;
+    }  
 }
 
 void loop()
