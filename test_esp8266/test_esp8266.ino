b1d675da9e6b5549d77ca5c8333d7641b2c9d4b0
Tuan Nguyen <Dungnhincainay@gmail.com>
2021-03-05 00:17:18 +0700

    Chinh sửa vài thứ

--- test_esp8266/test_esp8266.ino
+++ test_esp8266/test_esp8266.ino
@@ -9,49 +9,61 @@ char auth[] = "8TytNxie25BVKYolzRm_yLjF1vAGbrCA";
 char ssid[] = "Wifi Cua Tuan";
 char pass[] = "";
 
-float t=0;
-float h=0;
+float h=0;      // V4 - Do am khong khi
+float t=0;      // V5 - Nhiet do khong khi
+float hum=0;    // V6 - Do am Dat 
 int flag=0;
 
-/*
- * V1 V2 : Chọn chế độ
- * v7 Kp
- * V8 Ki
- * V9 Kd
- */
+
+ // V1 V2 : Chọn chế độ
+ float value1;
+ float Kp =0;      //Kp
+ float Ki =0;      //Ki
+ float Kd =0;      //Kd
+
 int ledState = LOW;
-float value1;
 unsigned long Ago = 0;
 unsigned long Now = 0;
+
 const long Set_time = 300;
+
+
 
 BLYNK_CONNECTED() {
     Blynk.syncAll();
 }
- BLYNK_WRITE(V1)
- {   
+ BLYNK_WRITE(V1){
    value1 = param.asFloat(); // Get value as integer
  }
-//Blynk.setProperty(V10, "url", 1, "https://i0.wp.com/www.makerlab.vn/wp-content/uploads/2020/04/pngbarn.png?fit=86%2C117&ssl=1");
+ BLYNK_WRITE(V7){   
+   Kp = param.asFloat(); // Get value as integer
+ }
+ BLYNK_WRITE(V8){   
+   Ki = param.asFloat(); // Get value as integer
+ }
+ BLYNK_WRITE(V9){   
+   Kd = param.asFloat(); // Get value as integer
+ }
+
 void setup()
 {
     Serial.begin(115200);
     Blynk.begin(auth, ssid, pass,"iot.htpro.vn", 8080);
     pinMode(LED_BUILTIN, OUTPUT);
-    timer.setInterval(500, sendUptime);
+    timer.setInterval(1500, sendUptime);
 }
 
 void sendUptime()
 {
-  h = h+0.845 ;             // Đọc giá tri nhiệt độ, độ ẩm không khí
-  t = t+1.23 ;              // Đọc giá tri nhiệt độ, độ ẩm không khí
-  if (h>=100){              // Đọc giá tri nhiệt độ, độ ẩm không khí
-    h=0;                    // Đọc giá tri nhiệt độ, độ ẩm không khí
-  }                         // Đọc giá tri nhiệt độ, độ ẩm không khí
-  if(t>=100){               // Đọc giá tri nhiệt độ, độ ẩm không khí
-    t=1;                    // Đọc giá tri nhiệt độ, độ ẩm không khí
-  }
-  Now = millis();                 // Nhấp nháy led
+   h = h+0.845 ;             // Đọc giá tri nhiệt độ, độ ẩm không khí
+   t = t+1.23 ;              // Đọc giá tri nhiệt độ, độ ẩm không khí
+   if (h>=100){              // Đọc giá tri nhiệt độ, độ ẩm không khí
+     h=0;                    // Đọc giá tri nhiệt độ, độ ẩm không khí
+   }                         // Đọc giá tri nhiệt độ, độ ẩm không khí
+   if(t>=100){               // Đọc giá tri nhiệt độ, độ ẩm không khí
+     t=1;                    // Đọc giá tri nhiệt độ, độ ẩm không khí
+   }
+   Now = millis();                // Nhấp nháy led
   if (Now - Ago >= Set_time) {    // Nhấp nháy led
     Ago = Now;                    // Nhấp nháy led
     if (ledState == LOW) {        // Nhấp nháy led
@@ -61,20 +73,20 @@ void sendUptime()
     }                             // Nhấp nháy led
     digitalWrite(LED_BUILTIN, ledState);       // Đảo trạng thái led
   }
-  Serial.println("\nKet qua do duoc:");       // Hiển thị trên monitor
-  Serial.print("Do Am = ");                     // Hiển thị trên monitor
-  Serial.print(h);                              // Hiển thị trên monitor
-  Serial.print("%  ");                          // Hiển thị trên monitor
-  Serial.print("Nhiet do = ");                  // Hiển thị trên monitor
-  Serial.print(t);                              // Hiển thị trên monitor
-  Serial.print("°C \n");
-  Serial.print("\nGia tri V1: ");                  // Hiển thị trên monitor
-  Serial.print(value1);                           // Hiển thị trên monitor
-  Serial.print(" ");                  // Hiển thị trên monitor
+   Serial.println("\nKet qua do duoc:");          // Hiển thị trên monitor
+   Serial.print("Do Am = ");                     // Hiển thị trên monitor
+   Serial.print(h);                             // Hiển thị trên monitor
+   Serial.print("%  ");                        // Hiển thị trên monitor
+   Serial.print("Nhiet do = ");               // Hiển thị trên monitor
+   Serial.print(t);                                   // Hiển thị trên monitor
+   Serial.print("°C \n");                            // Hiển thị trên monitor
+   Serial.print("\nGia tri V1: ");                  // Hiển thị trên monitor
+   Serial.print(value1);                           // Hiển thị trên monitor
+   Serial.print(" ");                             // Hiển thị trên monitor
 
-  Blynk.virtualWrite(V6, t);      // Gửi dữ liệu lên sever - lên Điện thoại.
+  Blynk.virtualWrite(V6, t);        // Gửi dữ liệu lên sever - lên Điện thoại.
   Blynk.virtualWrite(V5, h);      // Gửi dữ liệu lên sever - lên Điện thoại.
-  
+  //  Blynk.virtualWrite(V0, VIRTUAL_PIN_0); // ghi gia tri len App qua V0
   if(t>=70 && flag==0){
     Blynk.notify("Nhiệt độ cao");
     flag=1;
@@ -87,4 +99,6 @@ void loop()
 {
   Blynk.run();
   timer.run();
+
+  // bla bla.....
 }
